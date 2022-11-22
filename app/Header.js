2f4d92bbafbbc619a0ec83debe12abe9f054cc89
German Bobadilla <germanbobadilla@gmail.com>
2022-11-22 17:39:10 -0400

    Day 21

--- app/Header.js
+++ app/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header>
       <h1>I am the header</h1>
+      <nav>
+        <Link href="/contact">Home</Link>
+        <Link href="/contact">Projects</Link>
+        <Link href="/contact">Education</Link>
+        <Link href="/contact">Blog</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
     </header>
   );
 };
